1ea79ef9e2b071444d6ccad2f4683dfd6f69773a
NikkhahAli <aliii.nikkhah@gmail.com>
2024-07-13 13:39:58 +0330

    Update userControllers.js

--- Controllers/userControllers.js
+++ Controllers/userControllers.js
@@ -6,6 +6,8 @@ import {ApiFeatures} from './../Utils/apiFeatures.js'
 import bcryptjs from "bcryptjs"
 
 export const getAllUser = catchAsync (async (req , res , next) =>{
+    req.query.fields = "_id,phone,email,role"
+
     const features = new ApiFeatures(User , req.query).filters().sort().limit().paginate().populate()
     const getAll = await features.model 
     return res.status(200).json({
@@ -18,12 +20,18 @@ export const getOneUser = catchAsync (async (req , res , next) =>{
     const { id } = req.params 
     const { id : userId , role } = jwt.verify(req.headers.authorization.split(' ')[1] , process.env.JWT_PATH) 
     const user = await User.findById(id) 
-
+ 
     if (user._id === userId || role === "admin") {
         return res.status(200).json({
             status : "ok" ,
-            data : user
-        })
+            data : {
+                id : user._id ,
+                fullName : user.fullName ,
+                email : user.email ,
+                phone : user.phone ,
+                role : user.role ,
+            }
+        })  
     }
     else {
         return next (new HandleError("you cannot Access , You must be admin or user" , 401))
@@ -34,20 +42,20 @@ export const updateUser = catchAsync (async (req , res , next) =>{
     const { id } = req.params 
     const { id : userId , role } = jwt.verify(req.headers.authorization.split(' ')[1] , process.env.JWT_PATH) 
     const user = await User.findById(id) 
-    const { role : userRole , password , ...others } = req.body 
+
+    const { role : userRole = '' , password = '' , ...others } = req.body 
 
     let updateUsers ;
 
     if (user._id === userId || role === "admin") {
         const hashPassword=  bcryptjs.hashSync(password , 10)
         if (role === "admin") {
-            updateUsers = await User.findByIdAndUpdate(id , { password : hashPassword , ...others , role :userRole} , {new : true , runValidators : true})  
+            updateUsers = await User.findByIdAndUpdate(id , { password : password ? hashPassword : user.password , ...others , role : userRole ? userRole : user.role} , {new : true , runValidators : true})  
         }
         else {
-            updateUsers = await User.findByIdAndUpdate(id , { password : hashPassword , ...others} , {new : true , runValidators : true})  
-            
+            updateUsers = await User.findByIdAndUpdate(id , { password : password ? hashPassword : user.password , ...others} , {new : true , runValidators : true})  
         }
-        
+
         return res.status(200).json({
             status : "ok" ,
             message : "updated..." ,
